a57a3c04ef5103c631fca087221a9df413e77a85
Mike <micah41224@gmail.com>
2021-11-17 09:29:33 -0500

    Fixed syntax/formatting errors in schema file using feedback from workbench

--- db/schema.sql
+++ db/schema.sql
@@ -12,13 +12,19 @@ CREATE TABLE role_table (
     id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL FOREIGN KEY REFERENCES department_table(id)
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department_table(id)
 );
 
 CREATE TABLE employee_table (
     id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL FOREIGN KEY REFERENCES role_table(id), 
-    manager_id INT FOREIGN KEY REFERENCES employee_table(id)
+    role_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role_table(id), 
+    manager_id INT,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee_table(id)
 );
